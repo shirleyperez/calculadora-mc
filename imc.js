948cc0106a45c4b605304867c237fbdb6061b527
Shirleyperez <carolinesanchez918@gmail.com>
2017-07-17 13:24:08 -0500

    actualizado Heidy

---
+++ imc.js
@@ -0,0 +1,8 @@
+var  masa = prompt("ingresa tu masa corporal");
+var altura =  prompt("ingresa su altura");
+function IMC(masa,altura) {
+var resultado = alert("Tu resultado del IMC es ")
+var num = 5.56789;
+var n = (num.toFixed(2));
+}
+IMC(masa,altura);
